c35235212b9a26da7489e52dead9756efa0eb340
Denis Bourge <denis.bourge@sharingcloud.com>
2019-04-20 10:51:52 +0200

    refactor: moving _peripherals module in mod

--- src/peripherals/mod.rs
+++ src/peripherals/mod.rs
@@ -1,9 +1,37 @@
 //! Peripherals
 
-mod _peripherals;
 pub mod cartridge;
 pub mod input;
 pub mod memory;
 pub mod screen;
 
-pub use _peripherals::Peripherals;
+use self::input::InputState;
+use self::memory::Memory;
+use self::screen::Screen;
+
+/// Peripherals
+pub struct Peripherals {
+    /// Input
+    pub input: InputState,
+    /// Memory
+    pub memory: Memory,
+    /// Screen
+    pub screen: Screen,
+}
+
+impl Peripherals {
+    /// Create peripherals
+    pub fn new() -> Self {
+        Peripherals {
+            input: InputState::new(),
+            memory: Memory::new(),
+            screen: Screen::new(),
+        }
+    }
+}
+
+impl Default for Peripherals {
+    fn default() -> Self {
+        Self::new()
+    }
+}
